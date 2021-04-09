2d576475c22bc87ca55c62b74cf51d6d652f002e
hades4511 <muhammadusman6242@gmail.com>
2021-04-09 21:08:31 +0500

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
+const fileUpload = multer({dest: 'uploads/'});
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', fileUpload.single('upfile'), function (req, res, next) {
+  const file = req.file;
+  res.status(200).json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+  fs.unlinkSync(path.join(__dirname, 'uploads', file.filename));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
